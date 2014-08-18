a9318eb001f29450b83538654226013b0edb5849
Lis Kenny <liskenny@lis-kennys-imac.home>
2014-08-18 23:36:24 +0100

    Initial Commit

---
+++ cocoa test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cocoa test
+//
+//  Created by Lis Kenny on 18/08/2014.
+//  Copyright (c) 2014 Lis Kenny. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ cocoa test/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  cocoa test
+//
+//  Created by Lis Kenny on 18/08/2014.
+//  Copyright (c) 2014 Lis Kenny. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
